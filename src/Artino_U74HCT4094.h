b7e663ff8f2bd9480fd54981ce7698878f4e80e7
chenglong.liu <artint.liu@gmail.com>
2023-02-12 12:11:44 +0800

    *修改定义顺序

--- src/Artino_U74HCT4094.h
+++ src/Artino_U74HCT4094.h
@@ -1,44 +1,47 @@
 #ifndef _U74HCT4094_H_
 #define _U74HCT4094_H_
 
-class U74HCT4094
+namespace Artino
 {
-	uint8_t m_dataPin;
-	uint8_t m_clockPin;
-	uint8_t m_strobePin;
-	
-	public:
-	U74HCT4094(uint8_t clockPin, uint8_t dataPin, uint8_t strobePin)
-    : m_clockPin(clockPin)
-		, m_dataPin(dataPin)		
-		, m_strobePin(strobePin)
-	{
-	}
-	
-	void Begin()
-	{
-		pinMode(m_clockPin, OUTPUT);
-		pinMode(m_dataPin, OUTPUT);
-		pinMode(m_strobePin, OUTPUT);
-	}
+  class U74HCT4094
+  {
+    uint8_t m_dataPin;
+    uint8_t m_clockPin;
+    uint8_t m_strobePin;
 
-	void WriteByte(uint8_t c)
-	{
-    digitalWrite(m_dataPin, LOW);
-		digitalWrite(m_strobePin, LOW);
-    digitalWrite(m_clockPin, LOW);
+  public:
+    U74HCT4094(uint8_t clockPin, uint8_t dataPin, uint8_t strobePin)
+      : m_clockPin(clockPin)
+      , m_dataPin(dataPin)
+      , m_strobePin(strobePin)
+    {
+    }
 
-		shiftOut(m_dataPin, m_clockPin, MSBFIRST, c);
+    void Begin()
+    {
+      pinMode(m_clockPin, OUTPUT);
+      pinMode(m_dataPin, OUTPUT);
+      pinMode(m_strobePin, OUTPUT);
+    }
 
-		digitalWrite(m_strobePin, HIGH);
-    digitalWrite(m_strobePin, LOW);
-	}	
+    void WriteByte(uint8_t c)
+    {
+      digitalWrite(m_dataPin, LOW);
+      digitalWrite(m_strobePin, LOW);
+      digitalWrite(m_clockPin, LOW);
 
-	void WriteWord(uint16_t c)
-	{
-    WriteByte(c & 0xff);
-    WriteByte((c >> 8) & 0xff);
-	}	
-};
+      shiftOut(m_dataPin, m_clockPin, MSBFIRST, c);
+
+      digitalWrite(m_strobePin, HIGH);
+      digitalWrite(m_strobePin, LOW);
+    }
+
+    void WriteWord(uint16_t c) // 从最低位开始
+    {
+      WriteByte((c >> 8) & 0xff);
+      WriteByte(c & 0xff);
+    }
+  };
+}
 
 #endif // _U74HCT4094_H_
